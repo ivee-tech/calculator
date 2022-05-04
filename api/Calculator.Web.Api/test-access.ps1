6a313d09db01024db16a31c7ade9addeef09ddd1
Daniel Radu <daradu@microsoft.com>
2022-05-04 16:18:11 +1000

    Tokenise settings; added test-access

---
+++ api/Calculator.Web.Api/test-access.ps1
@@ -0,0 +1,9 @@
+﻿$expression = "1+2"
+$url = 'https://zz-calculator-dev-dr.azurewebsites.net/api/Operation/execute'
+$data = @{ expression = $expression }
+$contentType = 'application/json'
+$method = 'POST'
+$body = $data | ConvertTo-Json
+$result = Invoke-WebRequest -Uri $url -Body $body -Method $method -ContentType $contentType
+$result
+
