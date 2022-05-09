bfab7ad834e49ff45d2880a69be7700676df22ff
Daniel Radu <daradu@microsoft.com>
2022-05-09 16:15:00 +1000

    Added UnitTests project

--- api/Calculator.Web.Api/Controllers/OperationController.cs
+++ api/Calculator.Web.Api/Controllers/OperationController.cs
@@ -28,45 +28,11 @@ namespace Calculator.Web.Api.Controllers
         [HttpGet]
         public async Task<IActionResult> Get(DateTime sd, DateTime ed)
         {
-            var list = new List<OperationLog>();
-            var connectionString = GetConnectionString();
-            using (var conn = new SqlConnection(connectionString))
-            {
-                conn.Open();
-                using (var cmd = conn.CreateCommand())
-                {
-                    cmd.CommandText = "SELECT * FROM dbo.OperationLogs WHERE Date BETWEEN @sd AND @ed";
-                    cmd.Parameters.AddWithValue("@sd", sd);
-                    cmd.Parameters.AddWithValue("@ed", ed);
-                    var rdr = await cmd.ExecuteReaderAsync();
-                    while(rdr.Read())
-                    {
-                        int id = 0;
-                        string expression = "";
-                        double result = 0;
-                        DateTime date = DateTime.MinValue;
-                        string error = "";
-                        rdr.TryGetValue<int>("Id", ref id);
-                        rdr.TryGetValue<string>("Expression", ref expression);
-                        rdr.TryGetValue<double>("Result", ref result);
-                        rdr.TryGetValue<DateTime>("Date", ref date);
-                        rdr.TryGetValue<string>("Error", ref error);
-                        var log = new OperationLog()
-                        {
-                            Id = id,
-                            Expression = expression,
-                            Result = result,
-                            Date = date,
-                            Error = error
-                        };
-                        list.Add(log);
-                    }
-                }
-            }
+            var opSvc = new OperationService(_env.IsProduction(), _configuration);
+            var list = await opSvc.Get(sd, ed);
             return Ok(list);
         }
 
-        // POST api/<OperationLogController>
         [HttpPost("execute")]
         public async Task<IActionResult> ExecuteAndLogOperation(OperationRequest request)
         {
@@ -74,73 +40,15 @@ namespace Calculator.Web.Api.Controllers
             {
                 return BadRequest("Expression cannot be empty.");
             }
-            var expression = Uri.UnescapeDataString(request?.Expression);
-            var success = true;
-            var error = string.Empty;
-            double result = 0;
+            var opSvc = new OperationService(_env.IsProduction(), _configuration);
             try
             {
-                result = await Execute(expression);
+                var response = await opSvc.ExecuteAndLogOperation(request);
+                return Ok(response);
             }
             catch (Exception ex)
             {
-                success = false;
-                error = ex.ToString();
-            }
-            await LogOperation(expression, result, error);
-            if (success)
-            {
-                return Ok(new OperationResponse { Expression = expression, Result = result });
-            }
-            else
-            {
-                return Problem(error);
-            }
-        }
-
-        private string GetConnectionString()
-        {
-            if(_env.IsProduction())
-            {
-                var cred = new DefaultAzureCredential();
-                var kvUrl = $"https://{_configuration["Settings:KeyVaultName"]}.vault.azure.net";
-                var kvUri = new Uri(kvUrl);
-                var client = new SecretClient(kvUri, cred);
-                var connectionString = client.GetSecret("ConnectionStrings--Default").Value.Value;
-                return connectionString;
-            }
-            else
-            {
-                var connectionString = _configuration["ConnectionStrings:Default"];
-                return connectionString;
-            }
-        }
-
-        private async Task<double> Execute(string expression)
-        {
-            double result = 0;
-            var pattern = @"(?<n>(\d)+(\.\d+)*)";
-            var expr = Regex.Replace(expression, pattern, "(double)(${n})");
-            result = await CSharpScript.EvaluateAsync<double>(expr).ConfigureAwait(false);
-            return result;
-        }
-
-        private async Task LogOperation(string expression, double result, string error)
-        {
-            var connectionString = GetConnectionString();
-            using (var conn = new SqlConnection(connectionString))
-            {
-                conn.Open();
-                using (var cmd = conn.CreateCommand())
-                {
-                    var date = DateTime.Now;
-                    cmd.CommandText = "INSERT INTO dbo.OperationLogs(Expression, Result, Date, Error) VALUES(@Expression, @Result, @Date, @Error)";
-                    cmd.Parameters.AddWithValue("@Expression", expression);
-                    cmd.Parameters.AddWithValue("@Result", result);
-                    cmd.Parameters.AddWithValue("@Date", date);
-                    cmd.Parameters.AddWithValue("@Error", error);
-                    await cmd.ExecuteNonQueryAsync();
-                }
+                return Problem(ex.ToString());
             }
         }
 
